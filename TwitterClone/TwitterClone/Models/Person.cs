37085e82f6cd6435b44be651b40e64c2a54f6d61
Thiyagu Alagu <Thiyagu.Alagu@cognizant.com>
2019-09-19 04:44:15 -0700

    FSE - Commit

---
+++ TwitterClone/TwitterClone/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TwitterClone.Models
+{
+    public class Person
+    {
+        [DisplayName("User Id")]
+        public string User_id { get; set; }
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+        [DisplayName("Fullname")]
+        public string Fullname { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+    }
+}
